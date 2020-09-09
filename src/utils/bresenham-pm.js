e26a4b1ccbffcc65db820dca516f2a5eac8a1fe7
emanuel71jo <emanuel71jo@gmail.com>
2020-09-09 13:58:09 -0300

    Reta PM imprimindo nos oito octantes

--- src/utils/bresenham-pm.js
+++ src/utils/bresenham-pm.js
@@ -1,58 +1,58 @@
 export function PM(x1, y1, x2, y2, height, width) {
   var points = [];
-  var deltaX = x2 - x1;
-  var deltaY = y2 - y1;
+  var dX = x2 - x1;
+  var dY = y2 - y1;
   var x = x1;
   var y = y1;
 
-  var xIncr, yIncr, p, c1, c2;
+  var xIncr, yIncr, d, incrE, incrNE;
 
   var X = Math.round(x);
   var Y = Math.round(y);
 
   points.push(X / (width / 2), Y / (height / 2));
 
-  if (deltaX < 0) {
-    deltaX = -deltaX;
+  if (dX < 0) {
+    dX = -dX;
     xIncr = -1;
   } else {
     xIncr = 1;
   }
 
-  if (deltaY < 0) {
-    deltaY = -deltaY;
+  if (dY < 0) {
+    dY = -dY;
     yIncr = -1;
   } else {
     yIncr = 1;
   }
 
-  if (deltaX > deltaY) {
-    p = 2 * deltaY - deltaX;
-    c1 = 2 * deltaY;
-    c2 = 2 * (deltaY - deltaX);
+  if (dX > dY) {
+    d = 2 * dY - dX;
+    incrE = 2 * dY;
+    incrNE = 2 * (dY - dX);
 
-    for (var k = 1; k <= deltaX; k++) {
+    for (var k = 1; k <= dX; k++) {
       X += xIncr;
-      if (p < 0) {
-        p += c1;
+      if (d < 0) {
+        d += incrE;
       } else {
-        p += c2;
+        d += incrNE;
         Y += yIncr;
       }
       points.push(X / (width / 2), Y / (height / 2));
     }
   } else {
-    p = 2 * deltaX - deltaY;
-    c1 = 2 * deltaX;
-    c2 = 2 * (deltaX - deltaY);
+    d = 2 * dX - dY;
+    incrE = 2 * dX;
+    incrNE = 2 * (dX - dY);
 
-    for (var i = 1; i <= deltaY; i++) {
+    for (var i = 1; i <= dY; i++) {
       Y += yIncr;
-      if (p < 0) {
-        p += c1;
+      if (d < 0) {
+        d += incrE;
       } else {
-        p += c2;
-        x += xIncr;
+        d += incrNE;
+        X += xIncr;
       }
       points.push(X / (width / 2), Y / (height / 2));
     }
